558b265a61132e9a6863d0d42cd2a65a1056a35c
nagadeepakbhushan <nagadeepak000@gmail.com>
2021-03-06 12:05:54 +0530

    user-details-fetch-done

--- src/main/java/com/railways/login/repository/SessionRepository.java
+++ src/main/java/com/railways/login/repository/SessionRepository.java
@@ -14,4 +14,7 @@ public interface SessionRepository extends CrudRepository<Sessions,String> {
     @Modifying
     @Query(value = "delete from sessions where sessionid = ?1",nativeQuery = true)
     void deleteSession(String uname);
+
+    @Query(value = "Select user_name from sessions where sessionid=?1",nativeQuery = true)
+    String findUserNameBySessionId(String sessionid);
 }
