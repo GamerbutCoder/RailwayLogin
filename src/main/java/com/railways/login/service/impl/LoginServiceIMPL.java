cea9fad98a44b501899d5597bd19819a995bf37d
nagadeepakbhushan <nagadeepak000@gmail.com>
2021-03-08 01:36:24 +0530

    kafka replacing feign client

--- src/main/java/com/railways/login/service/impl/LoginServiceIMPL.java
+++ src/main/java/com/railways/login/service/impl/LoginServiceIMPL.java
@@ -1,5 +1,6 @@
 package com.railways.login.service.impl;
 
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import com.railways.login.client.ClientService;
 import com.railways.login.dto.LoginRequestDTO;
 import com.railways.login.dto.LoginResponseDTO;
@@ -9,17 +10,36 @@ import com.railways.login.repository.LoginRepository;
 import com.railways.login.repository.SessionRepository;
 import com.railways.login.service.LoginService;
 import com.railways.login.util.CustomHash;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
-
-
-
-import java.util.Optional;
 
 @Service
 public class LoginServiceIMPL implements LoginService {
+
+        public static Properties getPropertiesOfKafka(){
+            Properties props = new Properties();
+            props.put("bootstrap.servers", "localhost:9092");
+            props.put("acks", "all");
+            props.put("retries", 0);
+            props.put("linger.ms", 1);
+            props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            return props;
+        }
+
+
+
+
     @Autowired
     private LoginRepository loginRepository;
 
@@ -60,9 +80,10 @@ public class LoginServiceIMPL implements LoginService {
                     sessions.setUserName(requestDTO.getUserName());
                     sessionRepository.save(sessions);
 
-                clientService.setSessionInBookAndSearch(requestDTO.getUserName(),sessionID,"true");
+//                clientService.setSessionInBookAndSearch(requestDTO.getUserName(),sessionID,"true");
                 responseDTO.setMessage("SUCCESS");
                 responseDTO.setSessionID(sessionID);
+                kafkaMethod(requestDTO.getUserName(),sessionID+" "+"true");
                 return responseDTO;
             }
             else{
@@ -73,5 +94,10 @@ public class LoginServiceIMPL implements LoginService {
         responseDTO.setMessage("FAILED");
         responseDTO.setSessionID("");
         return responseDTO;
+    }
+    private void kafkaMethod(String userName ,String sessionID){
+        Producer<String,String> producer = new KafkaProducer<>(getPropertiesOfKafka());
+        producer.send(new ProducerRecord<>("sessions-kafka",userName,sessionID));
+        producer.close();
     }
 }
--- src/main/java/com/railways/login/service/impl/LogoutServiceIMPL.java
+++ src/main/java/com/railways/login/service/impl/LogoutServiceIMPL.java
@@ -5,11 +5,17 @@ import com.railways.login.dto.LogoutRequestDTO;
 import com.railways.login.entity.Sessions;
 import com.railways.login.repository.SessionRepository;
 import com.railways.login.service.LogoutService;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+
+import static com.railways.login.service.impl.LoginServiceIMPL.getPropertiesOfKafka;
+
 @Service
 public class LogoutServiceIMPL implements LogoutService {
     @Autowired
@@ -34,12 +40,19 @@ public class LogoutServiceIMPL implements LogoutService {
             }
             try{
                 //clientService.setSessionInBookAndSearch(requestDTO.getUserName(),"false");
-                clientService.deleteSesssionInBookAndSearch(requestDTO.getSessionId());
+                //clientService.deleteSesssionInBookAndSearch(requestDTO.getSessionId());
+                kafkaMethod("deleting user with this session ID",requestDTO.getSessionId());
+
             }
             catch (Exception e){
 
             }
 
         }
+    }
+    private void kafkaMethod(String userName ,String sessionID){
+        Producer<String,String> producer = new KafkaProducer<>(getPropertiesOfKafka());
+        producer.send(new ProducerRecord<>("sessions-kafka",userName,sessionID));
+        producer.close();
     }
 }
